83bf8a3e1f2ebdc3759fbf2931f2067c4fa58a17
Alvaro Videla <videlalvaro@gmail.com>
2012-05-16 23:54:34 +0200

    adds user management functionality

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
  */
 
 var express = require('express')
-, routes = require('./routes')
+, routes = require('./routes') //TODO separate routes in specific controllers
+, login = require('./routes/login.js')
 , resize = require('./lib/resize.js')
 ;
 
@@ -37,11 +38,37 @@ app.configure('production', function(){
     app.use(express.errorHandler());
 });
 
-// Routes
+//Code taken from express/examples/auth/app.js
+// app.locals.use(function(req,res){
+//     var err = req.session.error
+//         , msg = req.session.success;
+//     delete req.session.error;
+//     delete req.session.success;
+//     res.locals.message = '';
+//     if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+//     if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+// });
 
+function restrict(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.redirect('/login');
+    }
+}
+
+// Routes
 app.get('/', routes.index);
+app.get('/register', login.register);
+app.post('/register', login.addUser);
+app.get('/login', login.login);
+app.post('/login', login.auth);
+app.get('/logout', login.logout);
+
 app.post('/upload', routes.upload);
 app.get('/image/:id', routes.serveFile);
+//app.get('/images/:userid', routes.userImages);
 
 app.listen(3000, function(){
     resize.startConsumers();
---
+++ routes/login.js
@@ -0,0 +1,117 @@
+var bcrypt = require('bcrypt')
+var redis = require('redis')
+;
+
+function userKey(username) {
+    return "user:" + username;
+}
+
+// Used to generate a hash of the plain-text password + salt
+function hash(pass, salt) {
+    return bcrypt.hashSync(pass, salt);
+}
+
+function generateSalt() {
+    return bcrypt.genSaltSync(10);
+}
+
+// Authenticate using our plain-object database of doom!
+function authenticate(name, pass, fn) {
+    console.log('authenticating %s:%s', name, pass);
+    //get user from redis. User is a hash
+    var client = redis.createClient();
+    client.HGETALL([userKey(name)], function (err, obj) {
+        client.quit();
+        if (err || obj == null) {
+            console.log('cannot find user');
+            return fn(new Error('cannot find user'));
+        } else {
+            console.log("got reply: ", obj);
+            var user = {
+                name: obj.name.toString(),
+                salt: obj.salt.toString(),
+                pass: obj.pass.toString()
+            };
+            if (user.pass == hash(pass, user.salt)) return fn(null, user);
+            fn(new Error('invalid password'));
+        }
+    });
+}
+
+exports.register = function(req, res) {
+    //delivers registration form
+    //TODO move this to a method run before this route
+    if (req.session.user) {
+        res.redirect('/');
+        return;
+    }
+    res.render('register', { title: 'Cloudstagram - Register' });
+};
+
+exports.addUser = function(req, res) {
+    console.log("addUser: ", req.body.username, req.body.password);    
+    if (!req.session.user) {
+        var username = req.body.username, 
+        password = req.body.password;
+        var client = redis.createClient();
+        
+        client.exists(userKey(username), function(error, data) {
+            console.log("exists: ", data);
+            if (data != 1) {
+                var salt = generateSalt();
+                user = {
+                    name: username,
+                    salt: salt,
+                    pass: hash(password, salt)
+                };
+                client.hmset(userKey(username), user, function(error, reply) {
+                    if (reply == "OK") {
+                        req.session.regenerate(function(){
+                            client.quit();
+                            req.session.user = user;
+                            res.redirect('/');
+                        });
+                    } else {
+                        //TODO display error: can't create user
+                    }
+                });
+            } else {
+                //TODO display error: user already exists
+            }
+        });
+    } 
+    res.redirect('/');
+};
+
+exports.login = function(req, res) {
+    //serve login for
+    //TODO move this to a method run before this route
+    if (req.session.user) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login', { title: 'Cloudstagram - Login', error: req.session.error });
+};
+
+exports.auth = function(req, res) {
+    authenticate(req.body.username, req.body.password, function(err, user){
+        if (user) {
+            // Regenerate session when signing in
+            // to prevent fixation 
+            req.session.regenerate(function(){
+                req.session.user = user;
+                res.redirect('back');
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your '
+                + ' username and password.';
+            res.redirect('login');
+        }
+    });
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy(function() {
+        res.redirect('/');
+    });
+};
