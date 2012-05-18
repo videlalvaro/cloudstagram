9538e7aaeb7855e6342b69b2ceafb2cb71c9f37d
Alvaro Videla <videlalvaro@gmail.com>
2012-05-18 15:52:03 +0200

    improves timeago settings placement

--- public/javascripts/cloudstagram.js
+++ public/javascripts/cloudstagram.js
@@ -1,24 +1,23 @@
-jQuery.timeago.settings.strings = {
-    prefixAgo: null,
-    prefixFromNow: null,
-    suffixAgo: null,
-    suffixFromNow: null,
-    seconds: "%s",
-    minute: "about a minute",
-    minutes: "%dm",
-    hour: "about an hour",
-    hours: "%dh",
-    day: "a day",
-    days: "%dd",
-    month: "about a month",
-    months: "%dm",
-    year: "about a year",
-    years: "%dy",
-    wordSeparator: " ",
-    numbers: []
-};
-
 $(document).ready(function() {
+    jQuery.timeago.settings.strings = {
+        prefixAgo: null,
+        prefixFromNow: null,
+        suffixAgo: null,
+        suffixFromNow: null,
+        seconds: "%s",
+        minute: "about a minute",
+        minutes: "%dm",
+        hour: "about an hour",
+        hours: "%dh",
+        day: "a day",
+        days: "%dd",
+        month: "about a month",
+        months: "%dm",
+        year: "about a year",
+        years: "%dy",
+        wordSeparator: " ",
+        numbers: []
+    };
     jQuery("abbr.timeago").timeago();
     
     if (loggedin) {
