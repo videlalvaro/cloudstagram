7dc20e4064e14d6705c7ca5389b685ddb7a295b5
Alvaro Videla <videlalvaro@gmail.com>
2012-05-19 11:56:33 +0200

    implements like functionality

--- public/javascripts/cloudstagram.js
+++ public/javascripts/cloudstagram.js
@@ -22,15 +22,36 @@ $(document).ready(function() {
     
     if (loggedin) {
         jQuery(".userimage").dblclick(function (event){
-            var imageid = jQuery(event.target).attr('data');
+            var img = jQuery(event.target);
+            var imageid = img.attr('data-id');
+            
+            if (jQuery("#" + loggedinuser + "-liked-" + imageid).length > 0) {
+                console.log("likes pic already");
+                return;
+            }
+
             jQuery.post('/like/'+ imageid, function () {
                 //TODO display notification
+                var likesul = jQuery('#likes-' + imageid);
+
+                
+                console.log("heart exists?: ", jQuery('#heart-' + imageid).length);
+                if (jQuery('#heart-' + imageid).length == 0) {
+                    likesul.prepend('<span id="heart-' + imageid+ '" class="heart"><i class="icon-heart"></i></span>');
+                }
+
+                var likesHeart = jQuery('#heart-' + imageid);
+
+                likesHeart.after("<li><span id='" + loggedinuser + "-liked-" + imageid + "'>"
+                                + "<a href='/images/" + loggedinuser + "'>" + loggedinuser + "</a>"
+                                + "</span></li>");
+                
                 console.log("liked: ", imageid);
             });
-            
         });
 
         jQuery(".follow").click(function (event){
+            //TODO prevent user from following already followed users
             var userid = jQuery(event.target).attr('data');
             jQuery.post('/follow/' + userid, function () {
                 console.log("followed: ", userid);
