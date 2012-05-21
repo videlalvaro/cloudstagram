807c9b75f01eb81bb4ed1919ff4796904999890d
Alvaro Videla <videlalvaro@gmail.com>
2012-05-21 19:41:57 +0200

    changes upload folder

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.configure(
             layout: true
         });
         app.use(express.static(__dirname + '/public'));
-        app.use(express.bodyParser());
+        app.use(express.bodyParser({uploadDir: __dirname + '/uploads' }));
         app.use(express.cookieParser());
         app.use(express.session(getSessionOptions()));
         app.use(express.methodOverride());
