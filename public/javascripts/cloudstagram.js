617527b373949f4843468bd440dd2f59938866fd
Alvaro Videla <videlalvaro@gmail.com>
2012-05-24 19:44:50 +0200

    moves profile logic into main js file.

--- public/javascripts/cloudstagram.js
+++ public/javascripts/cloudstagram.js
@@ -198,5 +198,31 @@ jQuery(document).ready(function() {
         });
 
         jQuery(".userimage").dblclick(likeImage);
+
+        //user profile actions
+        if (typeof profileUser !== "undefined") {
+            if (profileUser != loggedinuser) {
+                jQuery.get('/isfollower/' + profileUser, function (data) {
+                    if (data == "YES") {
+                        $("#alreadyfollow").removeClass("hidden");
+                    } else {
+                        $("#followform").removeClass("hidden");
+                    }
+                });   
+            }
+
+            jQuery(".follow").click(function (event){
+                var userid = jQuery(event.target).attr('data-id');
+                jQuery.post('/follow/' + userid, function (data) {
+                    console.log("follow result: ", data);
+                    if (data == "OK") {
+                        jQuery("#followform").addClass('hidden');
+                        jQuery("#alreadyfollow").removeClass('hidden');
+                    } else {
+                        console.log("follow failed");
+                    }
+                })
+            });
+        }
     }
 });
