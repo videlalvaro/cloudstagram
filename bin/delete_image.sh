f9b14f9dc767257df338ac5a53f9b854a23bd388
Alvaro Videla <videlalvaro@gmail.com>
2012-05-24 23:23:04 +0200

    script to delete files from the CLI

---
+++ bin/delete_image.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+HOST="http://cloudstagram.cloudfoundry.com"
+
+while getopts d:i o
+do
+    case "$o" in
+        i) IMAGE="$OPTARG";;
+        d) HOST="http://localhost:3000";;
+        ?) echo "Usage: $0 [-d] [-i image] "
+            exit 1;;
+    esac
+done
+
+
+echo curl -X POST $HOST/delete/image/$IMAGE
