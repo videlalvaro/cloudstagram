88520a31dc29e9582e01cf03b515d2209aa88461
Alvaro Videla <videlalvaro@gmail.com>
2012-08-23 11:37:14 +0200

    passes the response object to the helper

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ exports.index = function(req, res) {
     // TODO remove magick numbers. Move them to configuraion
     var username = req.session.user ? req.session.user.name : null;
     var welcome_msg = view_helpers.getWelcomeMessage(req.session.user);
-    var callback = view_helpers.imageList(req.session, username, welcome_msg);
+    var callback = view_helpers.imageList(res, req.session, username, welcome_msg);
 
     if (username == null) {
         user_images.getLatestImages(0, 49, callback);
@@ -19,6 +19,6 @@ exports.latestImages = function(req, res) {
     // TODO remove magick numbers. Move them to configuraion
     var username = req.session.user ? req.session.user.name : null;
     var welcome_msg = 'latest';
-    var callback = view_helpers.imageList(req.session, username, welcome_msg);
+    var callback = view_helpers.imageList(res, req.session, username, welcome_msg);
     user_images.getLatestImages(0, 49, callback);
 };
