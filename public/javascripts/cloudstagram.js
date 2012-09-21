1d428979c007f5a251e7144a9af6cb96fdbc2600
Alvaro Videla <videlalvaro@gmail.com>
2012-09-21 15:21:34 +0200

    refactors user profile initialization

--- public/javascripts/cloudstagram.js
+++ public/javascripts/cloudstagram.js
@@ -201,48 +201,55 @@ function enableLikeButtons() {
     jQuery("button.like").live('click', likeImage);
 }
 
+function initFollowButton() {
+    if (profileUser != loggedinuser) {
+        jQuery.get('/isfollower/' + profileUser, function (data) {
+            if (data == "YES") {
+                $("#alreadyfollow").removeClass("hidden");
+            } else {
+                $("#followform").removeClass("hidden");
+            }
+        });   
+    }
+
+    jQuery(".follow").click(function (event){
+        var userid = jQuery(event.target).attr('data-id');
+        jQuery.post('/follow/' + userid, function (data) {
+            console.log("follow result: ", data);
+            if (data == "OK") {
+                jQuery("#followform").addClass('hidden');
+                jQuery("#alreadyfollow").removeClass('hidden');
+            } else {
+                console.log("follow failed");
+            }
+        })
+    });
+}
+
+function initUserProfile() {
+    //user profile actions
+    if (typeof profileUser !== "undefined") {
+        initFollowButton();
+    }   
+}
+
 jQuery(document).ready(function() {
 
     initInstanceInfoTooltip();
+
+    //TODO: FETCH IMAGES VIA AJAX
 
     //if we fetch images via ajax we need to call this once the images are loaded
     initTimeAgo(); 
     //if we fetch images via ajax we need to call this once the images are loaded
     initMasonry();
 
-    if (!loggedin) {
-        initSideForms();
-    }
-
     if (loggedin) {
         initUploadForm();
         initCommentField();
         enableLikeButtons();
-
-        //user profile actions
-        if (typeof profileUser !== "undefined") {
-            if (profileUser != loggedinuser) {
-                jQuery.get('/isfollower/' + profileUser, function (data) {
-                    if (data == "YES") {
-                        $("#alreadyfollow").removeClass("hidden");
-                    } else {
-                        $("#followform").removeClass("hidden");
-                    }
-                });   
-            }
-
-            jQuery(".follow").click(function (event){
-                var userid = jQuery(event.target).attr('data-id');
-                jQuery.post('/follow/' + userid, function (data) {
-                    console.log("follow result: ", data);
-                    if (data == "OK") {
-                        jQuery("#followform").addClass('hidden');
-                        jQuery("#alreadyfollow").removeClass('hidden');
-                    } else {
-                        console.log("follow failed");
-                    }
-                })
-            });
-        }
+        initUserProfile();
+    } else {
+        initSideForms();
     }
 });
